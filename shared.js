efda59cdf1e8271459a737cbb8700cb0c7f31d4d
maron <maronwhite6@gmail.com>
2019-04-07 17:29:57 +0300

    Add modal when plan button is clicked

---
+++ shared.js
@@ -0,0 +1,20 @@
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function () {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    });
+}
+
+backdrop.addEventListener("click", closeModal);
+
+modalNoButton.addEventListener("click", closeModal);
+
+function closeModal() {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+}
