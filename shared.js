b6e3c0882f8e5d30eb7b4bc7f97959df79923ae7
maron <maronwhite6@gmail.com>
2019-04-10 09:23:00 +0300

    Add responsive mobile menu

--- shared.js
+++ shared.js
@@ -2,19 +2,42 @@ var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function () {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.className = 'open'; // This overwrites  the complete list class 
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function () {
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
+    closeModal();
+});
 
-modalNoButton.addEventListener("click", closeModal);
+if (modalNoButton) {
+    modalNoButton.addEventListener("click", closeModal);
+}
 
 function closeModal() {
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    backdrop.classList.remove('open');
+    if (modal) {
+        modal.classList.remove('open');
+    }
+
 }
+
+toggleButton.addEventListener('click', function () {
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
